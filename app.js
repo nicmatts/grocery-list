8f70ea3fa0a1c89b8718f0c67bcecb6965b81923
Nicole M <nicmatts@gmail.com>
2014-05-05 11:57:28 -0500

    add jquery

--- app.js
+++ app.js
@@ -1,8 +1,61 @@
 $(document).ready(function() {
-  $('li').click(function() {
+  // Capture the value of the input form and the selected category.  Apply the value to the correct category on the list.
+  $("#submit").click(function() {
+    var value = $("input").val();
+    var item = "<li class='added'>" + value + "<span class='x'>x</span></li>";
+    function getCategory(){
+      //alert($('.category option:selected').text());
+      switch($('.category option:selected').text()){
+        case "Bakery":
+          $('.bakery').show();
+          $('.bakery-list').append(item);
+          break;
+
+        case "Dairy":
+          $('.dairy').show();
+          $('.dairy-list').append(item);
+          break;
+
+        case "Household":
+          $('.household').show();
+          $('.household-list').append(item);
+          break;
+
+        case "Ingredients":
+          $('.ingredients').show();
+          $('.ingredients-list').append(item);
+          break;
+
+        case "Meats":
+          $('.meats').show();
+          $('.meats-list').append(item);
+          break;
+
+        case "Produce":
+          $('.produce').show();
+          $('.produce-list').append(item);
+          break;
+
+        case "Everything Else":
+          $('.everything-else').show();
+          $('.everything-else-list').append(item);
+          break;
+      }
+    }
+    getCategory();
+    $('input').val('');
+  });
+
+  $('ul').on('click', '.added', function() {
     $(this).toggleClass('in-cart');
   });
-  $('span.x').click(function() {
+  
+  $('ul').on('click', '.x', function() {
     $(this).parent().remove();
+  });
+
+  $("#clear").click(function() {
+    $('li').remove();
+    $('.container').hide();
   });
 });
